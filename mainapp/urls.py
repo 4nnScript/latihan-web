7df062d6e6bacaaf24885d4db2c9ef162e505d4d
annisa handayani <annisa.nurhandayani@gmail.com>
2021-08-13 19:46:28 +0700

    belum ada review

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from . import views, api
 
 urlpatterns = [
     path('count/<int:angka>/', views.count),
@@ -17,5 +17,10 @@ urlpatterns = [
     path('example/', views.example),
     path('profile/', views.profile),
     path('second/', views.second_page),
+
+
+    path('api/category/get-all/', api.get_categories),
+    path('api/product/get-all/', api.get_products),
+    path('api/category/create/', api.create_categories),
     path('', views.landing_page),
 ]
